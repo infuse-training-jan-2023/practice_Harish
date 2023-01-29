238b0ce4981962d21035574a9239c68de8d0c64c
harishkavlekar <harishkavlekar1378@gmail.com>
2023-01-29 14:21:22 +0530

    added elementAt method

--- exercise2.rb
+++ exercise2.rb
@@ -9,9 +9,17 @@ class Exercise2
         end
         return length > 0  ? arr[start_pos,length] : "length should be greater than zero"
     end
+    def element_at(arr,index)
+        if arr.empty?
+            return "array is empty"
+        end
+        return index>=arr.length() || index<-(arr.length) ? "array index out of bound" : arr.at(index)
+    end
 end   
 
 input = [9, 5, 1, 2, 3, 4, 0, -1]
 
 exercise2_obj = Exercise2.new()
 puts exercise2_obj.start_and_length([],-9,2)
+puts exercise2_obj.element_at([],-9)
+
