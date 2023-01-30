b41565cb33d25ed4db591451855e65e59f8f6b49
harishkavlekar <harishkavlekar1378@gmail.com>
2023-01-30 16:25:08 +0530

    browser maximize fixed

--- docker-workshop/automation.rb
+++ docker-workshop/automation.rb
@@ -1,4 +1,6 @@
 require_relative "test_framework"
+require "base64"
+
 class AutomateWebpage
     attr_accessor :framework_obj    
     def initialize()
@@ -6,28 +8,23 @@ class AutomateWebpage
     end
 
     def visit_website(url)
-      begin
-        framework_obj.navigate_to_page(url)
-      rescue 
-        return "page not found"
-      end
+      framework_obj.navigate_to_page(url)
+      puts "navigating to web page done"
+    
     end
 
-    def check_login_status(usrname)
-      begin
+    def check_login_status(url,usrname)
+        framework_obj.navigate_to_page(url)
         framework_obj.click_element({xpath: "id(\"viewport\")/div[1]/div/div[2]/div[3]/div/button[2]"})
         sleep(5)
-        login_status=framework_obj.get_Element_Text({class: "login-title"})
+        login_status=framework_obj.get_Element_Text({xpath: "//*[@id=\"viewport\"]/div[4]/span/div/div/aside/div[2]/div/div/div/h2"})
         sleep(5)
-        return  "Hey #{usrname}"==login_status ? "user logged in" : "user not logged in"
-      rescue 
-        return "checking login status failed"
-      end
+        
+        puts   "Hey #{usrname}"==login_status ? "#{usrname} logged in" : "#{usrname} not logged in"
+        puts "check login status done"
     end
 
-    def login(url,email,pass)
-      begin
-        
+    def login(url,email,pass)        
         framework_obj.navigate_to_page(url)
         sleep(5)
         framework_obj.click_element({xpath: "id(\"viewport\")/div[1]/div/div[2]/div[3]/div/button[2]"})
@@ -37,14 +34,10 @@ class AutomateWebpage
         sleep(5)
         framework_obj.click_element({xpath: "id(\"viewport\")/div[4]/span/div/div/aside/div[2]/div/div/form/div/div/button"})
         sleep(5)
-        return "login successfully "
-      rescue
-        return "login  failed"
-      end
+      puts "login done"
     end
     
     def logout()
-      begin
         sleep(5)
         framework_obj.send_key({xpath: "/html/body"},:arrow_up)
         sleep(5)
@@ -56,36 +49,29 @@ class AutomateWebpage
         sleep(2)
         framework_obj.click_element({xpath: "id(\"viewport\")/div[1]/div/div[2]/div[3]/div/button[2]"})
         sleep(5)
-        return "logged out successfully"
-      rescue 
-        return "logged out failed"
-      end
+        puts "logged out done"
     end
     def add_to_wishlist() 
-      begin
         sleep(5)
         framework_obj.click_element({xpath: "//*[@id=\"viewport\"]/div[1]/div/div[2]/div[3]/div/button[1]/div"})
         sleep(5)
         framework_obj.click_element({xpath: "//*[@id=\"viewport\"]/div[1]/div/div[3]/div[2]/div[2]/div[3]/div[2]/div[1]/p"})
         sleep(5)
+        
         framework_obj.click_element({xpath: "//*[@id=\"category\"]/div[4]/div/div[1]/div[1]/div[1]/a/div/img"})
         sleep(5)
         framework_obj.click_element({xpath: "//*[@id=\"product__select-size-sizes\"]/div[3]/button"})
         sleep(3)
         framework_obj.click_element({xpath: "//*[@id=\"product\"]/div/div[1]/div[1]/div[2]/div/div[3]/div[2]/div/div[3]/div/div[1]/button[2]"})
-        return " successfully added to wishlist "
-        
-      rescue
-        return "add to wishlist failed"
-      end
+        puts "add to wishlist done"
     end
     def add_to_cart(url)
-      begin
-        sleep(10)
+      
+        sleep(5)
         framework_obj.navigate_to_page(url)
         sleep(5)
         framework_obj.click_element({xpath: "//*[@id=\"viewport\"]/div[1]/div/div[2]/div[3]/div/button[1]/div"})
-        sleep(5)
+        sleep(5)        
         framework_obj.click_element({xpath: "//*[@id=\"viewport\"]/div[1]/div/div[3]/div[2]/div[2]/div[3]/div[2]/div[1]/p"})
         sleep(5)
         framework_obj.click_element({xpath: "//*[@id=\"category\"]/div[4]/div/div[1]/div[1]/div[1]/a/div/img"})
@@ -93,11 +79,9 @@ class AutomateWebpage
         framework_obj.click_element({xpath: "//*[@id=\"product__select-size-sizes\"]/div[3]/button"})
         sleep(3)
         framework_obj.click_element({xpath: "//*[@id=\"product\"]/div/div[1]/div[1]/div[2]/div/div[3]/div[2]/div/div[3]/div/div[1]/button[1]"})
-        return "added to cart successfully"
-      rescue => exception
-      return "adding to cart failed"
-      end 
-    end 
+        puts "add to cart done" 
+    end
+     
 end
 
 
@@ -106,13 +90,13 @@ automateWebpage_obj=AutomateWebpage.new()
 email="wgyjhwjrqxonqinlvz@tmmcv.com"
 pass="Pass@1234"
 usrname="john"
-automateWebpage_obj.visit_website(url)
-puts automateWebpage_obj.check_login_status(usrname)
+puts automateWebpage_obj.visit_website(url)
+puts automateWebpage_obj.check_login_status(url,usrname)
 sleep(10)
 puts automateWebpage_obj.add_to_cart(url)
 
 puts automateWebpage_obj.login(url,email,pass)
 sleep(10)
 puts automateWebpage_obj.add_to_wishlist()
+
 puts automateWebpage_obj.logout()
-puts automateWebpage_obj.check_login_status() 
--- docker-workshop/test_framework.rb
+++ docker-workshop/test_framework.rb
@@ -2,22 +2,23 @@ require 'selenium-webdriver'
 class Driver
     attr_accessor :driver
     def initialize
+        # Selenium::WebDriver::Chrome.driver_path= "C:\\Users\\haris\\OneDrive\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe"
+        # @driver=Selenium::WebDriver.for :chrome
         Selenium::WebDriver::Chrome.driver_path= "/opt/chromedriver-109.0.5414.74/chromedriver"
         options = Selenium::WebDriver::Chrome::Options.new
         options.add_argument('--headless')
-        options.add_argument('--window-size =1920,1080')
+        options.add_argument('--window-size=1920,1080')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-setuid-sandbox')
         @driver = Selenium::WebDriver.for :chrome, options: options
     end
     def navigate_to_page(url)
         begin
-            driver.manage.window.maximize
+            
             driver.get(url)
             return driver.title
-          rescue 
-            return "page not found"
+        rescue => exception
+            puts exception.message
         end
     end
 
@@ -25,8 +26,8 @@ class Driver
         begin
             key , value = locator.first
             return driver.find_element(key => value)
-        rescue
-            puts "element not found"
+        rescue => exception
+            puts exception.message
         end
     end
 
@@ -36,16 +37,16 @@ class Driver
             selected.click
             return true
         rescue => exception
-            raise exception
+            puts exception.message
         end
     end
     def send_key(locator,text)
         key , value = locator.first
         begin
-             selected= select_element(locator)
-             return selected.send_keys(text)
+            selected= select_element(locator)
+            return selected.send_keys(text)
         rescue => exception
-            raise exception
+            puts exception.message
         end
     end
 
@@ -54,7 +55,7 @@ class Driver
             selected=select_element(locator)
             return selected.text
         rescue => exception
-            raise exception
+            puts exception.message
         end
     end  
     def displayed(locator)
@@ -62,9 +63,15 @@ class Driver
             selected=select_element(locator)
             return selected.displayed?
         rescue => exception
-            raise exception
+            puts exception.message
+        end
+    end
+    def screenshot()
+        begin
+            puts   driver.save_screenshot('screenshot.png')
+        rescue => exception
+            puts exception.message
         end
     end
 
 end
-
