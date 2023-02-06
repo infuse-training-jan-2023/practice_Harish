186add2a6aa64bcaaf9c14e3d13670dd6a0586f3
harishkavlekar <harishkavlekar1378@gmail.com>
2023-02-06 10:45:15 +0530

    added unit test for todoapi

--- session-harish/todoapi.py
+++ session-harish/todoapi.py
@@ -3,17 +3,16 @@ import json
 from flask import Flask, Response,request
 
 app = Flask(__name__)
-
 @app.route('/todo/<int:num>',methods =['GET'])
 def get_data(num):
     return get_todo(num)
-
-def get_todo(num):
+    
+    
+def get_todo(num):    
     if isinstance(num, int) and num in range(1,200):
         url = "https://jsonplaceholder.typicode.com/todos/{}".format(num)
         res = requests.get(url)
-        res_data=res.text
-        return Response(res_data , mimetype='application/json', status=200)
-    return Response('{"message": "Invalid number supplied"}' , mimetype='application/json', status=200)
+        return res.json() 
+    return {"message": "Invalid number supplied"}
 if __name__ == '__main__':
     app.run(debug=True, port=5000,host='0.0.0.0')
--- session-harish/unit-test/test_todoapi.py
+++ session-harish/unit-test/test_todoapi.py
@@ -1,7 +1,20 @@
 from todoapi import get_todo
 
+message_invalid ={"message": "Invalid number supplied"}
+message_valid={
+    "completed":0,
+    "id": 1,
+    "title": "delectus aut autem",
+    "userId": 1
+}
 
-def test_get_todo(mocker):
+def test_get_todo_invalid_case(mocker):
+    mock = mocker.patch('todoapi.get_todo', return_value = [])
+    res = get_todo(1000)
+    assert res['message'] == message_invalid['message']
+
+def test_get_todo_valid_case(mocker):
     mock = mocker.patch('todoapi.get_todo', return_value = [])
     res = get_todo(1)
-    assert res.status == "200 OK"
+    for key in res.keys():
+        assert res[key] == message_valid[key]
